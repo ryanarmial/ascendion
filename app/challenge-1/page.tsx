8029abb38aabe5cf2bcab8081f7f12ce00fd7f6a
Ryan Akbar <ryanakbar.armial@gmail.com>
2025-05-26 23:44:11 +0700

    add challenge 1 html

--- app/challenge-1/page.tsx
+++ app/challenge-1/page.tsx
@@ -1,7 +1,11 @@
 export default function Page() {
   return (
     <div>
-      <h1>Challenge 1</h1>
+      <h1>Adding Two Numbers</h1>
+      <input type="text" placeholder="First Number" />
+      <input type="number" placeholder="Second Number" />
+      <button>Add Two Number</button>
+      <p>Total:</p>
     </div>
   );
 }
