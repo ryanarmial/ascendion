c3912bdd5d4d2c6296e68e04700af1ff034f089a
Ryan Akbar <ryanakbar.armial@gmail.com>
2025-05-27 00:22:40 +0700

    add change input and button click functionality

--- app/challenge-1/page.tsx
+++ app/challenge-1/page.tsx
@@ -1,17 +1,32 @@
+'use client'
+
 import { useState } from 'react';
 import styles from './styles.module.css';
 
 export default function Page() {
-  const [numbers, setNumbers] = useState<number[]>([0, 0]);
+  const [inputNumbers, setInputNumbers] = useState<number[]>([0, 0]);
   const [total, setTotal] = useState<number>(0);
+
+  const onChangeNumber = (index: number, value: string | number) => {
+    setInputNumbers((prev) => {
+      const newNumbers = [...prev];
+      newNumbers[index] = Number(value);
+      return newNumbers;
+    });
+  }
+
+  const handleAddNumbers = () => {
+    setTotal(inputNumbers[0] + inputNumbers[1]);
+    setInputNumbers([0, 0]);
+  }
 
   return (
     <div className={styles.page}>
       <div className={styles.container}>
         <h2>Adding Two Numbers</h2>
-        <input value={numbers[0]} type="text" placeholder="First Number" />
-        <input value={numbers[1]} type="number" placeholder="Second Number" />
-        <button>Add Two Number</button>
+        <input value={inputNumbers[0] || ''} onChange={(e) => onChangeNumber(0, e.target.value)} type="text" placeholder="First Number" />
+        <input value={inputNumbers[1] || ''} onChange={(e) => onChangeNumber(1, e.target.value)} type="number" placeholder="Second Number" />
+        <button onClick={handleAddNumbers}>Add Two Number</button>
         <p>Total: {total}</p>
       </div>
     </div>
