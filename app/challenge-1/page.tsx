e8f66d872171d54be396d5b9d38ad4f6c2df2f0d
Ryan Akbar <ryanakbar.armial@gmail.com>
2025-05-27 00:43:37 +0700

    feat: validate number

--- app/challenge-1/page.tsx
+++ app/challenge-1/page.tsx
@@ -4,29 +4,30 @@ import { useState } from 'react';
 import styles from './styles.module.css';
 
 export default function Page() {
-  const [inputNumbers, setInputNumbers] = useState<number[]>([0, 0]);
+  const [inputNumbers, setInputNumbers] = useState<Array<number | string>>(['', '']);
   const [total, setTotal] = useState<number>(0);
 
   const onChangeNumber = (index: number, value: string | number) => {
     setInputNumbers((prev) => {
       const newNumbers = [...prev];
-      newNumbers[index] = Number(value);
+      const isNumber = !isNaN(Number(value));
+      if(isNumber) newNumbers[index] = value;
       return newNumbers;
     });
   }
 
   const handleAddNumbers = () => {
-    setTotal(inputNumbers[0] + inputNumbers[1]);
-    setInputNumbers([0, 0]);
+    setTotal(Number(inputNumbers[0]) + Number(inputNumbers[1])) ;
+    setInputNumbers(['', '']);
   }
 
   return (
     <div className={styles.page}>
       <div className={styles.container}>
         <h2>Adding Two Numbers</h2>
-        <input value={inputNumbers[0] || ''} onChange={(e) => onChangeNumber(0, e.target.value)} type="text" placeholder="First Number" />
-        <input value={inputNumbers[1] || ''} onChange={(e) => onChangeNumber(1, e.target.value)} type="number" placeholder="Second Number" />
-        <button disabled={!inputNumbers[0] || !inputNumbers[1]} onClick={handleAddNumbers}>Add Two Number</button>
+        <input value={inputNumbers[0]} onChange={(e) => onChangeNumber(0, e.target.value)} type="text" placeholder="First Number" />
+        <input value={inputNumbers[1]} onChange={(e) => onChangeNumber(1, e.target.value)} type="number" placeholder="Second Number" />
+        <button onClick={handleAddNumbers}>Add Two Number</button>
         <p>Total: {total}</p>
       </div>
     </div>
