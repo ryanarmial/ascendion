a33905607840b95485ba5235f7f6c1b28fc2c201
Ryan Akbar <ryanakbar.armial@gmail.com>
2025-05-27 00:11:24 +0700

    add useState

--- app/challenge-1/page.tsx
+++ app/challenge-1/page.tsx
@@ -1,14 +1,18 @@
+import { useState } from 'react';
 import styles from './styles.module.css';
 
 export default function Page() {
+  const [numbers, setNumbers] = useState<number[]>([0, 0]);
+  const [total, setTotal] = useState<number>(0);
+
   return (
     <div className={styles.page}>
       <div className={styles.container}>
         <h2>Adding Two Numbers</h2>
-        <input type="text" placeholder="First Number" />
-        <input type="number" placeholder="Second Number" />
+        <input value={numbers[0]} type="text" placeholder="First Number" />
+        <input value={numbers[1]} type="number" placeholder="Second Number" />
         <button>Add Two Number</button>
-        <p>Total:</p>
+        <p>Total: {total}</p>
       </div>
     </div>
   );
