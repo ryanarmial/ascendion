6a84453be5b712aac3a0b627169dd762233a6dc6
Ryan Akbar <ryanakbar.armial@gmail.com>
2025-05-28 00:54:56 +0700

    add icon and set hide and show menu

--- app/challenge-2/page.tsx
+++ app/challenge-2/page.tsx
@@ -1,7 +1,11 @@
+'use client';
+
+import { useState } from 'react';
 import IconSearch from './IconSearch';
 import styles from './style.module.css';
 
 export default function Page() {
+  const [isOpenMenu, setIsOpenMenu] = useState<boolean>(false);
 
   return (
     <div className={styles.page}>
@@ -11,18 +15,20 @@ export default function Page() {
             <a className={styles.logo} href="/">ASCENDION</a>
             <div className={styles.icon_wrapper}>
               <IconSearch/>
-              <div className={`${styles.icon_menu} ${styles.active}`}/>
+              <div onClick={() => setIsOpenMenu(!isOpenMenu)} className={`${styles.icon_menu} ${isOpenMenu ? styles.open_menu : ''}`}/>
             </div>
           </div>
-          <ul className={`${styles.nav_links} ${styles.active}`}>
-            <li><a href="/">Showcase</a></li>
-            <li><a href="/">Docs</a></li>
-            <li><a href="/">Blog</a></li>
-            <li><a href="/">Analytics</a></li>
-            <li><a href="/">Commerce</a></li>
-            <li><a href="/">Templates</a></li>
-            <li><a href="/">Enterprise</a></li>
-          </ul>
+          <div className={`${styles.nav_links} ${isOpenMenu ? styles.active : ''}`}>
+            <ul>
+              <li><a href="/">Showcase</a></li>
+              <li><a href="/">Docs</a></li>
+              <li><a href="/">Blog</a></li>
+              <li><a href="/">Analytics</a></li>
+              <li><a href="/">Commerce</a></li>
+              <li><a href="/">Templates</a></li>
+              <li><a href="/">Enterprise</a></li>
+            </ul>
+          </div>
           {/* <div>
             <input className={styles.search} type="text" placeholder="Search Documentation..." />
           </div> */}
