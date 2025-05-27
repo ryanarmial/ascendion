337df001830e01f7b7d661f3905f6dfe6f927c59
Ryan Akbar <ryanakbar.armial@gmail.com>
2025-05-28 00:26:45 +0700

    refactor style nav link menu

--- app/challenge-2/page.tsx
+++ app/challenge-2/page.tsx
@@ -4,27 +4,29 @@ import styles from './style.module.css';
 export default function Page() {
 
   return (
-    <div className={styles.header}>
-      <div className={styles.container}>
-        <div className={styles.main_header}>
-          <a className={styles.logo} href="/">ASCENDION</a>
-          <div className={styles.icon_wrapper}>
-            <IconSearch/>
-            <div className={`${styles.icon_menu} ${styles.active}`}/>
+    <div className={styles.page}>
+      <div className={styles.header}>
+        <div className={styles.container}>
+          <div className={styles.main_header}>
+            <a className={styles.logo} href="/">ASCENDION</a>
+            <div className={styles.icon_wrapper}>
+              <IconSearch/>
+              <div className={`${styles.icon_menu} ${styles.active}`}/>
+            </div>
           </div>
+          <ul className={`${styles.nav_links} ${styles.active}`}>
+            <li><a href="/">Showcase</a></li>
+            <li><a href="/">Docs</a></li>
+            <li><a href="/">Blog</a></li>
+            <li><a href="/">Analytics</a></li>
+            <li><a href="/">Commerce</a></li>
+            <li><a href="/">Templates</a></li>
+            <li><a href="/">Enterprise</a></li>
+          </ul>
+          {/* <div>
+            <input className={styles.search} type="text" placeholder="Search Documentation..." />
+          </div> */}
         </div>
-        {/* <ul className={styles.nav_links}>
-          <li><a href="/">Showcase</a></li>
-          <li><a href="/">Docs</a></li>
-          <li><a href="/">Blog</a></li>
-          <li><a href="/">Analytics</a></li>
-          <li><a href="/">Commerce</a></li>
-          <li><a href="/">Templates</a></li>
-          <li><a href="/">Enterprise</a></li>
-        </ul>
-        <div>
-          <input className={styles.search} type="text" placeholder="Search Documentation..." />
-        </div> */}
       </div>
     </div>
   )
