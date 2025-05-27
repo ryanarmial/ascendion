569bd597d1b47295c3a1e4d3c1de835651976383
Ryan Akbar <ryanakbar.armial@gmail.com>
2025-05-28 03:18:22 +0700

    create html

--- app/challenge-3/page.tsx
+++ app/challenge-3/page.tsx
@@ -25,31 +25,26 @@ import { useState } from 'react';
 import styles from '../form.module.css';
 
 export default function Page() {
-  const [inputNumbers, setInputNumbers] = useState<Array<number | string>>(['', '']);
-  const [total, setTotal] = useState<number>(0);
+  const [inputNumbers, setInputNumbers] = useState<string>('');
+  const [inputTarget, setInputTarget] = useState<number|undefined>(undefined);
+  const [indexNumbers, setIndexNumbers] = useState<number[]>(0);
 
-  const onChangeNumber = (index: number, value: string | number) => {
-    setInputNumbers((prev) => {
-      const newNumbers = [...prev];
-      const isNumber = !isNaN(Number(value));
-      if(isNumber) newNumbers[index] = value;
-      return newNumbers;
-    });
-  }
-
-  const handleAddNumbers = () => {
-    setTotal(Number(inputNumbers[0]) + Number(inputNumbers[1])) ;
-    setInputNumbers(['', '']);
+  const handleGetIndexNumber = () => {
+    const numbers = inputNumbers.split(',').map(num => Number(num.trim()));
+    console.log(numbers, inputTarget);
+    // setTotal(Number(inputNumbers[0]) + Number(inputNumbers[1])) ;
+    // setInputNumbers(['', '']);
   }
 
   return (
     <div className={styles.page}>
       <div className={styles.container}>
-        <h2>Adding Two Numbers</h2>
-        <input value={inputNumbers[0]} onChange={(e) => onChangeNumber(0, e.target.value)} type="text" placeholder="First Number" />
-        <input value={inputNumbers[1]} onChange={(e) => onChangeNumber(1, e.target.value)} type="number" placeholder="Second Number" />
-        <button onClick={handleAddNumbers}>Add Two Number</button>
-        <p>Total: {total}</p>
+        <h2>Two Sum Numbers</h2>
+        <label htmlFor="">use comma as a separator. e.g: 1,2,3</label>
+        <input value={inputNumbers} onChange={(e) => setInputNumbers(e.target.value)} type="text" placeholder="Array of Number" />
+        <input value={inputTarget} onChange={(e) => setInputTarget(Number(e.target.value))} type="number" placeholder="Target Number" />
+        <button onClick={handleGetIndexNumber}>Get Two Number Index</button>
+        <p>Index Number: {indexNumbers}</p>
       </div>
     </div>
   );
